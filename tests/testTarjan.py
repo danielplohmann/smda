a0cb696aa009b6137183d33f0a201272ddb70965
pnx@pyrite <daniel.plohmann@fkie.fraunhofer.de>
2020-05-29 15:38:31 +0200

    bugfix

--- tests/testTarjan.py
+++ tests/testTarjan.py
@@ -17,16 +17,16 @@ class TarjanTestSuite(unittest.TestCase):
     """Provoke recursion"""
 
     def testInstructionEscaping(self):
-        test_data = {i: [] for i in range(10000)}
-        for i in range(1, 10000):
-            for j in range(i + 1, 10000, 1):
+        test_data = {i: [] for i in range(1000)}
+        for i in range(1, 1000):
+            for j in range(i + 1, 1000, 1):
                 test_data[i].append(j)
-        test_data[10000] = []
+        test_data[1000] = []
 
         tarjan = Tarjan(test_data)
         tarjan.calculateScc()
         sccs = tarjan.getResult()
-        self.assertEqual(10001, len(sccs))
+        self.assertEqual(1001, len(sccs))
 
 
 if __name__ == '__main__':
