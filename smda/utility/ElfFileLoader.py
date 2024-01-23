2e78e21165e292103dd30e843c73fa23798cf7ab
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2024-01-23 14:56:57 +0100

    improvements to ELF parsing

--- smda/utility/ElfFileLoader.py
+++ smda/utility/ElfFileLoader.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 
 LOGGER = logging.getLogger(__name__)
@@ -17,6 +18,15 @@ def align(v, alignment):
         return v
     else:
         return v + (alignment - remainder)
+    
+
+def has_bogus_sections(elffile, base_addr=0):
+    max_virtual_address = 0
+    for section in elffile.sections:
+        if section.virtual_address:
+            max_virtual_address = max(max_virtual_address, section.size + section.virtual_address)
+    if (max_virtual_address - base_addr) > sys.maxsize:
+        return True
 
 
 class ElfFileLoader(object):
@@ -35,9 +45,20 @@ class ElfFileLoader(object):
         #
         base_addr = 0
         candidates = [0xFFFFFFFFFFFFFFFF]
-        for section in elffile.sections:
-            if section.virtual_address:
-                candidates.append(section.virtual_address - section.offset)
+        if not has_bogus_sections(elffile):
+            for section in elffile.sections:
+                if section.virtual_address:
+                    addr = section.virtual_address - section.offset
+                    if addr >= 0:
+                        candidates.append(addr)
+        else:
+            # go for segments only instead
+            base_addr = 0
+            candidates = [0xFFFFFFFFFFFFFFFF]
+            for segment in elffile.segments:
+                if not segment.virtual_address:
+                    continue
+                candidates.append(segment.virtual_address)
         if len(candidates) > 1:
             base_addr = min(candidates)
         return base_addr
@@ -69,20 +90,27 @@ class ElfFileLoader(object):
         min_raw_offset = 0xFFFFFFFFFFFFFFFF
 
         # find begin of the first section/segment and end of the last section/segment.
-        for section in sorted(elffile.sections, key=lambda section: section.size, reverse=True):
-            if not section.virtual_address:
-                continue
-
-            max_virtual_address = max(max_virtual_address, section.size + section.virtual_address)
-            min_virtual_address = min(min_virtual_address, section.virtual_address)
-            min_raw_offset = min(min_raw_offset, section.file_offset)
-
+        if not has_bogus_sections(elffile):
+            for section in sorted(elffile.sections, key=lambda section: section.size, reverse=True):
+                if not section.virtual_address:
+                    continue
+                LOGGER.debug(f"ELF: section: 0x{section.virtual_address:x} 0x{section.size:x} 0x{section.file_offset:x}")
+                max_virtual_address = max(max_virtual_address, section.size + section.virtual_address)
+                min_virtual_address = min(min_virtual_address, section.virtual_address)
+                min_raw_offset = min(min_raw_offset, section.file_offset)
+        else:
+            LOGGER.warn(f"ELF: found possibly bogus section information, trying to parse segments.")
+        # parse segments regardless
         for segment in elffile.segments:
             if not segment.virtual_address:
                 continue
+            LOGGER.debug(f"ELF: segment: 0x{segment.virtual_address:x} 0x{segment.virtual_size:x} 0x{segment.file_offset:x}")
             max_virtual_address = max(max_virtual_address, segment.virtual_size + segment.virtual_address)
             min_virtual_address = min(min_virtual_address, segment.virtual_address)
             min_raw_offset = min(min_raw_offset, segment.file_offset)
+
+        if (max_virtual_address - base_addr) > sys.maxsize:
+            LOGGER.warn(f"ELF: found possibly bogus segment information, trying to parse segments.")
 
         if not max_virtual_address:
             LOGGER.debug("ELF: no section or segment data")
@@ -92,6 +120,7 @@ class ElfFileLoader(object):
         # offset 0x0 corresponds to the ELF base address
         virtual_size = max_virtual_address - base_addr
         LOGGER.debug("ELF: max virtual section offset: 0x%x", max_virtual_address)
+        LOGGER.debug("ELF: min virtual section offset: 0x%x", min_virtual_address)
         LOGGER.debug("ELF: mapped size: 0x%x", virtual_size)
         LOGGER.debug("ELF: min raw offset: 0x%x", min_raw_offset)
         mapped_binary = bytearray(align(virtual_size, 0x1000))
@@ -110,21 +139,31 @@ class ElfFileLoader(object):
                 continue
             rva = segment.virtual_address - base_addr
             LOGGER.debug("ELF: mapping segment of 0x%04x bytes at 0x%08x-0x%08x (0x%08x)", segment.physical_size, rva, rva + segment.physical_size, segment.virtual_address)
-            assert len(segment.content) == segment.physical_size
-            mapped_binary[rva:rva + segment.physical_size] = segment.content
+            if len(segment.content) != segment.physical_size:
+                LOGGER.warn("ELF: Mismatch in segment content vs. header-specified physical size!")
+                if len(segment.content) < segment.physical_size:
+                    LOGGER.warn("ELF: Padding to physical size with zeroes!")
+                    mapped_binary[rva:rva + len(segment.content)] = segment.content 
+                    mapped_binary[rva + len(segment.content):rva + segment.physical_size] = b"\x00" * (segment.physical_size - len(segment.content))
+                else:
+                    LOGGER.warn("ELF: More content than physical size!? Aborting, please report this case. :)")
+                    raise AssertionError("Received more content than physical size, which should never be possible?")
+            else:
+                mapped_binary[rva:rva + segment.physical_size] = segment.content
 
         # map sections.
         # may overwrite some segment data, but we expect the content to be identical.
-        for section in sorted(elffile.sections, key=lambda section: section.size, reverse=True):
-            if not section.virtual_address:
-                continue
-            rva = section.virtual_address - base_addr
-            LOGGER.debug("ELF: mapping section of 0x%04x bytes (content: 0x%04x bytes) at 0x%08x-0x%08x (0x%08x)", section.size, len(section.content), rva, rva + section.size, section.virtual_address)
-            # potentially perform zero padding if we have less content than section size
-            content_to_be_mapped = bytearray(section.content)
-            if len(section.content) < section.size:
-                content_to_be_mapped += b"\x00" * (section.size - len(section.content))
-            mapped_binary[rva:rva + section.size] = content_to_be_mapped
+        if not has_bogus_sections(elffile, base_addr):
+            for section in sorted(elffile.sections, key=lambda section: section.size, reverse=True):
+                if not section.virtual_address:
+                    continue
+                rva = section.virtual_address - base_addr
+                LOGGER.debug("ELF: mapping section of 0x%04x bytes (content: 0x%04x bytes) at 0x%08x-0x%08x (0x%08x)", section.size, len(section.content), rva, rva + section.size, section.virtual_address)
+                # potentially perform zero padding if we have less content than section size
+                content_to_be_mapped = bytearray(section.content)
+                if len(section.content) < section.size:
+                    content_to_be_mapped += b"\x00" * (section.size - len(section.content))
+                mapped_binary[rva:rva + section.size] = content_to_be_mapped
 
         # map header.
         # we consider the headers to be any data found before the first section/segment
@@ -174,5 +213,14 @@ class ElfFileLoader(object):
                 if section_size % section.alignment != 0:
                     section_size += section.alignment - (section_size % section.alignment)
                 section_end = section_start + section_size
-                code_areas.append([section_start, section_end])              
+                code_areas.append([section_start, section_end])    
+        for segment in sorted(elffile.segments, key=lambda segment: segment.physical_size, reverse=True):
+            # SHF_EXECINSTR = 4
+            if segment.flags & 0x4:
+                segment_start = segment.virtual_address
+                segment_size = segment.virtual_size
+                if segment_size % segment.alignment != 0:
+                    segment_size += segment.alignment - (segment_size % segment.alignment)
+                segment_end = segment_start + segment_size
+                code_areas.append([segment_start, segment_end])    
         return ElfFileLoader.mergeCodeAreas(code_areas)
