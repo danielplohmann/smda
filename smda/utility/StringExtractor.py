81f799d57731c30238eae8c4a02e1e08b71c59d3
r0ny123 <r0ny123@users.noreply.github.com>
2026-05-07 02:20:04 +0530

    Optimize printable string detection

--- smda/utility/StringExtractor.py
+++ smda/utility/StringExtractor.py
@@ -4,6 +4,8 @@ from typing import Iterator, Tuple
 
 from smda.common import SmdaFunction
 
+_IS_PRINTABLE_CHAR_CODE = tuple(chr(char) in string.printable for char in range(127))
+
 # ported back from our PR to capa v4.0.0
 # https://github.com/mandiant/capa/blob/v4.0.0/capa/features/extractors/smda/insn.py
 
@@ -64,7 +66,7 @@ def detect_ascii_len(smda_report, offset, maxlen=None):
     char = smda_report.buffer[rva]
     while (
         char < 127
-        and chr(char) in string.printable
+        and _IS_PRINTABLE_CHAR_CODE[char]
         and (maxlen is None or ascii_len < maxlen)
         and rva + 1 < len(smda_report.buffer)
     ):
@@ -87,7 +89,7 @@ def detect_unicode_len(smda_report, offset, maxlen=None):
     second_char = smda_report.buffer[rva + 1]
     while (
         char < 127
-        and chr(char) in string.printable
+        and _IS_PRINTABLE_CHAR_CODE[char]
         and second_char == 0
         and (maxlen is None or unicode_len < 2 * maxlen)
         and rva + 3 < len(smda_report.buffer)
